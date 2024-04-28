3227b106be9202479c97812551193f595c21568c
GuidoBergman <74883160+GuidoBergman@users.noreply.github.com>
2024-04-28 20:04:01 -0300

    Print attentions

--- training_evaluation/models.py
+++ training_evaluation/models.py
@@ -63,12 +63,13 @@ class mBERTBase(torch.nn.Module):
 class XLMRobertaBase(torch.nn.Module):
     def __init__(self):
         super(XLMRobertaBase, self).__init__()
-        self.l1 = transformers.XLMRobertaModel.from_pretrained('xlm-roberta-base', return_dict=False)
+        self.l1 = transformers.XLMRobertaModel.from_pretrained('xlm-roberta-base', return_dict=False, output_attentions=True)
         self.l2 = torch.nn.Dropout(0.3)
         self.l3 = torch.nn.Linear(768, COUNT_TECHNIQUES)
 
     def forward(self, ids, mask, token_type_ids):
-        _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        _, output_1, attentions = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids, output_attentions=True)
+        print('Attentions: ', attentions)
         output_2 = self.l2(output_1)
         output = self.l3(output_2)
         return output
